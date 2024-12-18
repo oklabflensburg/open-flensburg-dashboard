87cac29901c24ffb13ce5fbf7519c881bea0fc20
Chantal Butenberg <chantal.butenberg@gmail.com>
2024-12-18 21:24:12 +0100

    clean code

--- src/services/energyService.ts
+++ src/services/energyService.ts
@@ -6,71 +6,6 @@ import { transformSolarData } from "../utils/transformData";
 const API_URL = 'https://api.oklabflensburg.de'; //import.meta.env.VITE_API_URL ||
 
 //flensburg municipal key = 01001000
-/*
-export const fetchSolarData = async (): Promise<SolarAPIData[]> => {
-  try {
-    const response = await fetch(`${API_URL}/energy/v1/unit/solar/key?municipality_key=01001000`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error('Failed to fetch energy data:', error);
-    throw error;
-  }
-};
-
-export const fetchWindData = async (): Promise<WindAPIData[]> => {
-    try {
-      const response = await fetch(`${API_URL}/energy/v1/unit/wind/key?municipality_key=01001000`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Failed to fetch energy data:', error);
-      throw error;
-    }
-  };
-
-  export const fetchBiomassData = async (): Promise<WindAPIData[]> => {
-    try {
-      const response = await fetch(`${API_URL}/energy/v1/unit/biomass/key?municipality_key=01001000`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Failed to fetch energy data:', error);
-      throw error;
-    }
-  };
-
-  export const fetchNuclearData = async (): Promise<NuclearAPIData[]> => {
-    try {
-      const response = await fetch(`${API_URL}/energy/v1/unit/nuclear/key?municipality_key=01001000`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Failed to fetch energy data:', error);
-      throw error;
-    }
-  };
-
-  export const fetchCombustionData = async (): Promise<CompustionAPIData[]> => {
-    try {
-      const response = await fetch(`${API_URL}/energy/v1/unit/combustion/key?municipality_key=01001000`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Failed to fetch energy data:', error);
-      throw error;
-    }
-  };*/
 
   export const fetchEnergyData = async (energy_unit, municipality_key): Promise<CompustionAPIData[]> => {
 
@@ -81,7 +16,7 @@ export const fetchWindData = async (): Promise<WindAPIData[]> => {
         }
         return await response.json();
       } catch (error) {
-        console.error('Failed to fetch energy data:', error);
+        console.error(`Failed to fetch ${energy_unit} data:`, error);
         throw error;
       }
   };
